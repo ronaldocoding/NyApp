594aa9ab1f16a540a4331f8b7d4ad12501f1e08f
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-04-06 07:49:13 -0400

    Add toolbar component

---
+++ app/src/main/java/com/example/nyapp/books/BooksActivity.kt
@@ -0,0 +1,20 @@
+package com.example.nyapp.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nyapp.databinding.ActivityBooksBinding
+
+class BooksActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBooksBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBooksBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.toolbarMain.title = "Books"
+        setSupportActionBar(binding.toolbarMain)
+    }
+}
