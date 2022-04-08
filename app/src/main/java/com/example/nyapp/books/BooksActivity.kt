a7491438a7f258f4eb8d0564eec1874d02c02e03
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-04-08 09:06:45 -0400

    Add recycler view to BooksActivity

--- app/src/main/java/com/example/nyapp/books/BooksActivity.kt
+++ app/src/main/java/com/example/nyapp/books/BooksActivity.kt
@@ -2,7 +2,10 @@ package com.example.nyapp.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nyapp.R
+import com.example.nyapp.data.model.Book
 import com.example.nyapp.databinding.ActivityBooksBinding
 
 class BooksActivity : AppCompatActivity() {
@@ -17,5 +20,20 @@ class BooksActivity : AppCompatActivity() {
 
         binding.toolbarMain.title = getString(R.string.books_title)
         setSupportActionBar(binding.toolbarMain)
+
+        with(binding.recyclerviewBooks) {
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = BooksAdapter(getBooks())
+        }
+    }
+
+    private fun getBooks() : List<Book> {
+        return listOf<Book>(
+            Book("Title 1", "Author 1"),
+            Book("Title 2", "Author 2"),
+            Book("Title 3", "Author 3"),
+            Book("Title 4", "Author 4")
+        )
     }
 }
